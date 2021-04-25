02789b10564255921b4b5aeffc7624608ebaefa3
kokoo <jhko960717@gmail.com>
2021-04-25 11:52:01 +0900

    람다 기본식, 메소드 레퍼런스 예제 추가

---
+++ src/com/kokoo/lambda/example/Calculator.java
@@ -0,0 +1,6 @@
+package com.kokoo.lambda.example;
+
+public interface Calculator {
+    public int calc(int a, int b, int oper);
+
+}
---
+++ src/com/kokoo/lambda/example/Printer.java
@@ -0,0 +1,5 @@
+package com.kokoo.lambda.example;
+
+public interface Printer {
+    public void printArgStr(String s);
+}
